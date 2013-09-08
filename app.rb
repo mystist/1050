99fc7df515ada562abfa034e2e443672319bee7b
Liber <gjl87910lq@hotmail.com>
2013-09-08 22:39:39 +0800

    home

--- app.rb
+++ app.rb
@@ -4,9 +4,13 @@ require 'sinatra/activerecord'
 require 'redcarpet'
 
 get '/' do
-  "Hi there! It's initialize!"
+  erb :index
 end
 
 get '/dev-blog' do
   markdown :dev_blog, :layout_engine => :erb, :layout => :dev_blog_layout
+end
+
+get '/modification' do
+  erb :modification
 end
