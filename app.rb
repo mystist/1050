46d344f466e0e4c582c3633421354c0481b5e09a
Liber <gjl87910lq@hotmail.com>
2014-04-03 16:48:29 +0800

    fixbug_etag

--- app.rb
+++ app.rb
@@ -49,7 +49,7 @@ get '/dev-blog' do
 end
 
 get '/songs' do
-  @etag = Song.maximum('updated_at').strftime('%Y%m%d%H%M%S') + '/' + Song.count.to_s
+  @etag = Song.maximum('updated_at').to_s + '/' + Song.count.to_s
   last_modified @etag
   etag @etag
   songs = Song.all.order('`index`')
